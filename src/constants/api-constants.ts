860520ac1dc776d174cac109398fc4a1fb172e13
Praveen Sahu <72389100+praveen7512@users.noreply.github.com>
2024-10-20 18:57:43 +0530

    added base_url
    none

--- src/constants/api-constants.ts
+++ src/constants/api-constants.ts
@@ -8,7 +8,7 @@ export enum ApiMethods {
 export const API_KEY = "abhijeetchehakvivek"
 
 export const API_PATHS = {
-    BASE_URL: "http://13.127.168.173:3000",
+    BASE_URL: "http://43.204.219.136:3000",
     POST_CATEGORY: "/api/v1/category/",
     POST_PRODUCTS: "/api/v1/product/"
 };
